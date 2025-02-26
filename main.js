5b279df8e2f94fdea362b5b9ab6ab2aa94e4954c
Nakoki <naoki.sarrias@uvic.cat>
2025-02-26 09:31:49 +0100

    js

---
+++ main.js
@@ -0,0 +1,58 @@
+let songs = [
+    {
+        "name":"Eram, Sam, Sam",
+        "duration":136,
+        "img":"./eramsamsam.png",
+        "source":"./eramsamsam.mp3",
+        "playing":false,
+        "elapsed":0
+    },
+    {
+        "name":"Mac, Mec, Mic!",
+        "duration":95,
+        "img":"./macmecmic.png",
+        "source":"./macmecmic.mp4",
+        "playing":true,
+        "elapsed":0
+    },
+    {
+        "name":"Una Aventura Misteriosa!",
+        "duration":105,
+        "img":"./db.png",
+        "source":"./db.mp3",
+        "playing":false,
+        "elapsed":0
+    },
+    {
+        "name":"Uptown Girl",
+        "duration":186,
+        "img":"./tomatic.png",
+        "source":"./uptowngirl.mp3",
+        "playing":false,
+        "elapsed":0
+    },
+    {
+        "name":"We Are!",
+        "duration":171,
+        "img":"./weare.png",
+        "source":"./weare.mp3",
+        "playing":false,
+        "elapsed":0
+    },
+    {
+        "name":"Alltimelow",
+        "duration":217,
+        "img":"./fortnite.png",
+        "source":"./dekupaptamdem.mp3",
+        "playing":false,
+        "elapsed":0
+    },
+    {
+        "name":"",
+        "duration":136,
+        "img":"./.png",
+        "source":"./.mp3",
+        "playing":false,
+        "elapsed":0
+    },
+]
