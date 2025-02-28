077c683c35148f5efd025465ebd0025e137fe6d0
GenGomez <gomgen02@gmail.com>
2025-02-28 10:29:01 +0100

    canvis no funciona

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let audioActiu;
 
 function selectSong(song){
     audioActiu = song.getElementsByTagName("audio")[0];
+    let index = audioActiu.getAttribute("data-index");
+    resetplaying();
+    songs[index].playing = true;
+    localStorage.setItem("songs",JSON.stringify(songs));
+    song.classList.add("active");
 }
 
 function playpause(){
@@ -14,4 +19,12 @@ function next(){
 
 function previus(){
 
+}
+
+function resetplaying(){
+    let listSongs = document.getElementsByClassName("listsong");
+    listSongs.forEach((e,i) => {
+        e.classList.remove("active");
+        songs[i].playing = false;
+    });
 }
